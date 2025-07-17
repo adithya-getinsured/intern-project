59dceed315bacf1b172f325b4253ad404cddf87c
Adithya V <adithya.venkateswaran@getinsured.com>
2025-07-17 17:50:48 +0530

    Update chat URL

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,2 +1,2 @@
 export const AUTH_SERVICE_URL = 'https://chatbox-auth-service.onrender.com/api/auth';
-export const CHAT_SERVICE_URL = 'https://chatbox-auth-service.onrender.com/api/chat';
+export const CHAT_SERVICE_URL = 'https://chatbox-chat-service.onrender.com/api/chat';
